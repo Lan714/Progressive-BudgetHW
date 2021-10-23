3e99472aac74719b4a3afac1065393404ca13883
Lan714 <87465185+Lan714@users.noreply.github.com>
2021-10-23 14:13:25 -0700

    server updated

--- server.js
+++ server.js
@@ -1,28 +1,44 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+require('dotenv').config()
 
-const PORT = 3000;
+const express = require('express')
+const { join } = require('path')
 
-const app = express();
+const passport = require('passport')
+const { Strategy: LocalStrategy } = require('passport-local')
+const { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt')
 
-app.use(logger("dev"));
+const { User } = require('./models')
+const syncDB = require('./db')
 
-app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const app = express()
 
-app.use(express.static("public"));
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+app.use(passport.initialize())
+app.use(passport.session())
 
-// routes
-app.use(require("./routes/api.js"));
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.SECRET
+}, ({ id }, cb) => User.findById(id)
+  .populate('transactions')
+  .then(user => cb(null, user))
+  .catch(err => console.log(err))))
+
+app.use(require('./routes'))
+
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, 'public', 'index.html'))
+})
+
+const PORT = process.env.PORT || 3000
+
+syncDB()
+  .then(() => app.listen(PORT))
+  .catch(err => console.log(err))
